383cdf8ea19c50eb225fc75b39a26a5ed9beff4a
zl6481033 <398191005@qq.com>
2019-03-28 18:34:39 +0800

    有效的字母异位词

---
+++ String/242. Valid Anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> m;
+        for(char a:s) ++m[a];
+        for(int i=0;i<t.size();++i){
+            if(--m[t[i]]<0) return false;
+        }
+        return true;
+        
+    }
+};
