d4b9674b61016b4e1d6e6afb142ec56e28b88f8c
zl6481033 <398191005@qq.com>
2019-04-19 19:14:13 +0800

    “将有序数组转为二叉树”

---
+++ Tree/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedfun(nums,0,(int)nums.size()-1);
+    }
+    TreeNode* sortedfun(vector<int>& nums,int left,int right){
+        if(left>right) return NULL;
+        int mid=(left+right)/2;
+        TreeNode* cur=new TreeNode(nums[mid]);
+        cur->left=sortedfun(nums,left,mid-1);
+        cur->right=sortedfun(nums,mid+1,right);
+        return cur;
+    }
+};
