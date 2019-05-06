10c2c521f33448ae493162658b82834d116e264a
zl6481033 <398191005@qq.com>
2019-05-06 16:23:17 +0800

    3的幂

---
+++ Math/326. Power of Three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while (n && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+ 
+ 
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return (n > 0 && 1162261467 % n == 0);
+    }
+};
