910e68cefe9820b88e3ba0f4b710e8041cc12209
zhangli6481033 <398191005@qq.com>
2019-06-14 18:10:53 +0800

    复杂链表的使用

---
+++ 剑指offer/25、复杂链表的复制.cpp
@@ -0,0 +1,66 @@
+/*
+struct RandomListNode {
+    int label;
+    struct RandomListNode *next, *random;
+    RandomListNode(int x) :
+            label(x), next(NULL), random(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    
+   
+    void CloneNode(RandomListNode* pHead){
+        RandomListNode* pNode = pHead;
+        while(pNode != NULL){
+            RandomListNode* pCloned = new RandomListNode(0);
+            pCloned->label = pNode->label;
+            pCloned->next = pNode->next;
+            pCloned->random = NULL;
+            
+            pNode->next = pCloned;
+            pNode = pCloned->next;
+        }
+    }
+    
+   
+    void ConnectNodes(RandomListNode* pHead){
+        RandomListNode* pNode = pHead;
+        while(pNode != NULL){
+            RandomListNode* pCloned = pNode->next;
+            if(pNode->random != NULL){
+                pCloned->random = pNode->random->next;
+            }
+            pNode = pCloned->next;
+        }
+    }
+    
+    
+    RandomListNode* ReconnectNodes(RandomListNode* pHead){
+        RandomListNode* pNode = pHead;
+        RandomListNode* pClonedHead = NULL;
+        RandomListNode* pClonedNode = NULL;
+        
+        if(pNode != NULL){
+            pClonedHead = pClonedNode = pNode->next;
+            pNode->next = pClonedNode->next;
+            pNode = pNode->next;
+        }
+        
+        while(pNode != NULL){
+            pClonedNode->next = pNode->next;
+            pClonedNode = pClonedNode->next;
+            pNode->next = pClonedNode->next;
+            pNode = pNode->next;
+        }
+        return pClonedHead;
+    }
+    
+    RandomListNode* Clone(RandomListNode* pHead)
+    {
+        CloneNode(pHead);
+        ConnectNodes(pHead);
+        return ReconnectNodes(pHead);
+    }
+};
